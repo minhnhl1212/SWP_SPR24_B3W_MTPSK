5b39fa188dc61f456ad4a3917c7bb6b2f13e50ba
Shymastic <shymastic@gmail.com>
2024-04-18 15:44:28 +0700

    cap nhat order va orderdetail lai

--- KidStore/src/java/DTO/Order.java
+++ KidStore/src/java/DTO/Order.java
@@ -4,24 +4,37 @@ import java.util.Date;
 
 /**
  *
- * @author admin
+ * @author trant
  */
-public class Order {
-    
-    private int userId;
-    private int toyId;
-    private int quantity;
-    private double amount;
+public class OrderDetail {
     private int orderId;
+    private int userId;
+    private int statusId;
+    private Date createDate;
+    private int voucherId;
+    private int cardId;
+    private int paymentId;
+    private int toyId;
 
-    public Order() {
+    public OrderDetail() {
     }
 
-    public Order(int userId, int toyId, int quantity, double amount, int orderId) {
+    public OrderDetail(int orderId, int userId, int statusId, Date createDate, int voucherId, int cardId, int paymentId, int toyId) {
+        this.orderId = orderId;
         this.userId = userId;
+        this.statusId = statusId;
+        this.createDate = createDate;
+        this.voucherId = voucherId;
+        this.cardId = cardId;
+        this.paymentId = paymentId;
         this.toyId = toyId;
-        this.quantity = quantity;
-        this.amount = amount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
         this.orderId = orderId;
     }
 
@@ -33,6 +46,46 @@ public class Order {
         this.userId = userId;
     }
 
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getVoucherId() {
+        return voucherId;
+    }
+
+    public void setVoucherId(int voucherId) {
+        this.voucherId = voucherId;
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
     public int getToyId() {
         return toyId;
     }
@@ -40,29 +93,5 @@ public class Order {
     public void setToyId(int toyId) {
         this.toyId = toyId;
     }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
+  
 }
--- KidStore/src/java/DTO/OrderDetail.java
+++ KidStore/src/java/DTO/OrderDetail.java
@@ -4,37 +4,24 @@ import java.util.Date;
 
 /**
  *
- * @author trant
+ * @author admin
  */
-public class OrderDetail {
-    private int orderId;
+public class Order {
+    
     private int userId;
-    private int statusId;
-    private Date createDate;
-    private int voucherId;
-    private int cardId;
-    private int paymentId;
     private int toyId;
+    private int quantity;
+    private double amount;
+    private int orderId;
 
-    public OrderDetail() {
+    public Order() {
     }
 
-    public OrderDetail(int orderId, int userId, int statusId, Date createDate, int voucherId, int cardId, int paymentId, int toyId) {
-        this.orderId = orderId;
+    public Order(int userId, int toyId, int quantity, double amount, int orderId) {
         this.userId = userId;
-        this.statusId = statusId;
-        this.createDate = createDate;
-        this.voucherId = voucherId;
-        this.cardId = cardId;
-        this.paymentId = paymentId;
         this.toyId = toyId;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
+        this.quantity = quantity;
+        this.amount = amount;
         this.orderId = orderId;
     }
 
@@ -46,46 +33,6 @@ public class OrderDetail {
         this.userId = userId;
     }
 
-    public int getStatusId() {
-        return statusId;
-    }
-
-    public void setStatusId(int statusId) {
-        this.statusId = statusId;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public int getVoucherId() {
-        return voucherId;
-    }
-
-    public void setVoucherId(int voucherId) {
-        this.voucherId = voucherId;
-    }
-
-    public int getCardId() {
-        return cardId;
-    }
-
-    public void setCardId(int cardId) {
-        this.cardId = cardId;
-    }
-
-    public int getPaymentId() {
-        return paymentId;
-    }
-
-    public void setPaymentId(int paymentId) {
-        this.paymentId = paymentId;
-    }
-
     public int getToyId() {
         return toyId;
     }
@@ -93,5 +40,29 @@ public class OrderDetail {
     public void setToyId(int toyId) {
         this.toyId = toyId;
     }
-  
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
 }
