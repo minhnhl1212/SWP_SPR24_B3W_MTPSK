5b73778cf76994d1f57af23fb4f1956e40050915
tranthaison493 <sonttse171846@fpt.edu.vn>
2024-04-26 09:31:26 +0700

    Fix điều hướng và load dữ liệu manager

---
+++ KidStore/src/java/Controller/LoadManagerProductController.java
@@ -0,0 +1,55 @@
+package Controller;
+
+import DAO.ToyDAO;
+import DTO.Toy;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author trant
+ */
+public class LoadManagerProductController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            HttpSession session = request.getSession();
+            ToyDAO toyDAO = new ToyDAO();
+            ArrayList<Toy> toyList = toyDAO.toyList();
+            session.setAttribute("TOY_LIST", toyList);
+            RequestDispatcher rd = request.getRequestDispatcher("ManagerProduct.jsp");
+            rd.forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(LoadManagerProductController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
